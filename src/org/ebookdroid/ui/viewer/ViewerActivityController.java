89710458ba5488a347214275a6daaa29052a9d2c
ieee8023 <joseph@josephpcohen.com>
2015-08-22 22:44:43 -0400

    fixed autorotation!

--- src/org/ebookdroid/ui/viewer/ViewerActivityController.java
+++ src/org/ebookdroid/ui/viewer/ViewerActivityController.java
@@ -147,7 +147,7 @@ public class ViewerActivityController extends AbstractActivityController<ViewerA
     public void beforeCreate(final ViewerActivity activity) {
         final AppSettings newSettings = AppSettings.current();
 
-        activity.setRequestedOrientation(newSettings.rotation.getOrientation());
+        //activity.setRequestedOrientation(newSettings.rotation.getOrientation());
         IUIManager.instance.setTitleVisible(activity, newSettings.showTitle, true);
 
         TouchManager.loadFromSettings(newSettings);
@@ -163,7 +163,7 @@ public class ViewerActivityController extends AbstractActivityController<ViewerA
     public void beforeRecreate(final ViewerActivity activity) {
         final AppSettings newSettings = AppSettings.current();
 
-        activity.setRequestedOrientation(newSettings.rotation.getOrientation());
+        //activity.setRequestedOrientation(newSettings.rotation.getOrientation());
         IUIManager.instance.setTitleVisible(activity, newSettings.showTitle, true);
 
         TouchManager.loadFromSettings(newSettings);
@@ -830,13 +830,13 @@ public class ViewerActivityController extends AbstractActivityController<ViewerA
     public void onAppSettingsChanged(final AppSettings oldSettings, final AppSettings newSettings,
             final AppSettings.Diff diff) {
         final ViewerActivity activity = getManagedComponent();
-        if (diff.isRotationChanged()) {
-            if (bookSettings != null) {
-                activity.setRequestedOrientation(bookSettings.getOrientation(newSettings));
-            } else {
-                activity.setRequestedOrientation(newSettings.rotation.getOrientation());
-            }
-        }
+//        if (diff.isRotationChanged()) {
+//            if (bookSettings != null) {
+//                activity.setRequestedOrientation(bookSettings.getOrientation(newSettings));
+//            } else {
+//                activity.setRequestedOrientation(newSettings.rotation.getOrientation());
+//            }
+//        }
 
         if (diff.isFullScreenChanged()) {
             IUIManager.instance.setFullScreenMode(activity, activity.view.getView(), newSettings.fullScreen);
@@ -888,9 +888,9 @@ public class ViewerActivityController extends AbstractActivityController<ViewerA
             }
         }
 
-        if (diff.isRotationChanged()) {
-            getManagedComponent().setRequestedOrientation(newSettings.getOrientation(AppSettings.current()));
-        }
+//        if (diff.isRotationChanged()) {
+//            getManagedComponent().setRequestedOrientation(newSettings.getOrientation(AppSettings.current()));
+//        }
 
         if (diff.isFirstTime()) {
             getZoomModel().initZoom(newSettings.getZoom());
